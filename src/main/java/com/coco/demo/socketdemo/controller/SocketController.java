d0e59e05d10323e7c5009b8d58ed88cd737aca24
wangzz <81303012@qq.com>
2019-02-26 10:12:52 +0800

    websocket代码优化

--- src/main/java/com/coco/demo/socketdemo/controller/SocketController.java
+++ src/main/java/com/coco/demo/socketdemo/controller/SocketController.java
@@ -6,15 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author wangzz
+ */
 @RestController
 public class SocketController {
 
     @Autowired
     private WebSocket webSocket;
 
-    @RequestMapping("wangzz")
+    @RequestMapping("/")
     public String sendMessage() {
-        webSocket.sendMessage("hello websocket!");
-        return "发送成功！";
+        return "启动成功！";
     }
+
+    @RequestMapping("send")
+    public String send(String msg) {
+        webSocket.sendMessage("msg:{" + msg + "}");
+        return "消息发送成功！消息：{" + msg + "}";
+    }
+
+    @RequestMapping("close")
+    public String close() {
+        webSocket.onClose();
+        return "关闭连接成功！";
+    }
+
 }
--- src/main/java/com/coco/demo/socketdemo/service/WebSocket.java
+++ src/main/java/com/coco/demo/socketdemo/service/WebSocket.java
@@ -8,6 +8,7 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
@@ -26,23 +27,31 @@ public class WebSocket {
     public void onOpen(Session session) {
         this.session = session;
         webSocketSet.add(this);
-        log.info("【websocket消息】 有新的连接，总数：{}", webSocketSet.size());
+        log.error("【websocket消息】 有新的连接，总数：{}", webSocketSet.size());
     }
 
     @OnClose
     public void onClose() {
         webSocketSet.remove(this);
-        log.info("【websocket消息】 连接断开，总数：{}", webSocketSet.size());
+        log.error("【websocket消息】 连接断开，总数：{}", webSocketSet.size());
     }
 
     @OnMessage
     public void onMessage(String message) {
-        log.info("【websocket消息】 收到客户端发来的消息：{}", message);
+        log.error("【websocket消息】 收到客户端发来的消息：{}", message);
+
+        // 接受到消息之后在发送到前台
+        try {
+            session.getBasicRemote().sendText("【websocket消息】 收到客户端发来的消息：" + message);
+        } catch (IOException e) {
+            log.error("【websocket消息】发送异常~");
+            e.printStackTrace();
+        }
     }
 
     public void sendMessage(String message) {
         for (WebSocket webSocket : webSocketSet) {
-            log.info("【websocket消息】 广播消息，message={}", message);
+            log.error("【websocket消息】 广播消息，message={}", message);
             try {
                 webSocket.session.getBasicRemote().sendText(message);
             } catch (Exception e) {
